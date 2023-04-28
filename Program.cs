c54742485283c6519918f4482812222e7cadd58a
Aiden York <alyork@crimson.ua.edu>
2023-04-28 00:55:25 -0500

    2nd commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,105 @@
-﻿
+﻿using mis_221_pa_5_alyork2;
+
+//Set variables
+
+int input;
+int input2;
+string name;
+string ID;
+string mailingAddress;
+string emailAddress;
+
+
+//Menu
+
+Console.WriteLine("Welcome to Train Like a Champion - Personal Fitness");
+Console.WriteLine();
+Console.WriteLine("Would you like to manage trainer data (1), manage listing data (2), manage customer booking data (3), run reports (4), or exit(5)");
+input = int.Parse(Console.ReadLine());
+
+while(input != 5)
+{
+    if(input == 1)
+    {
+        Console.WriteLine("Would you like to add (1), edit (2), or delete (3) a trainer (Press 4 for none of the options).");
+        input2 = int.Parse(Console.ReadLine());
+        while(input2 != 4)
+        {
+            if(input2 == 1)
+            {
+                Console.WriteLine("Please enter the name of the new trainer");
+                name = Console.ReadLine();
+                Trainer newTrainer = new Trainer(name);
+            }
+        
+            else if(input2 == 2)
+            {
+                Console.WriteLine("What is the information for the trainer that you would like to edit, first enter name");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Now ID ");
+                ID = (Console.ReadLine());
+
+                Console.WriteLine("Now mailing address");
+                mailingAddress = Console.ReadLine();
+
+                Console.WriteLine("And now email address");
+                emailAddress = Console.ReadLine();
+
+
+                int newID = int.Parse(ID);
+
+                Trainer oldTrainer = new Trainer(newID, name, mailingAddress, emailAddress);
+                oldTrainer.GetName();
+                oldTrainer.GetID();
+                oldTrainer.GetMailingAddress();
+                oldTrainer.GetEmailAddress();
+
+                oldTrainer.EditTrainer(ID, name, mailingAddress, emailAddress);
+            }
+            else if(input2 == 3)
+            {
+                Console.WriteLine("What is the information for the trainer that you would like to delete, first enter name");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Now ID ");
+                ID = (Console.ReadLine());
+
+                Console.WriteLine("Now mailing address");
+                mailingAddress = Console.ReadLine();
+
+                Console.WriteLine("And now email address");
+                emailAddress = Console.ReadLine();
+
+
+                int newID = int.Parse(ID);
+
+                Trainer oldTrainer = new Trainer(newID, name, mailingAddress, emailAddress);
+                oldTrainer.GetName();
+                oldTrainer.GetID();
+                oldTrainer.GetMailingAddress();
+                oldTrainer.GetEmailAddress();
+
+                oldTrainer.DeleteTrainer(ID, name, mailingAddress, emailAddress);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Would you like to add (1), edit (2), or delete (3) a trainer (Press 4 for none of the options).");
+            input2 = int.Parse(Console.ReadLine());
+        }
+        if(input == 2)
+        {
+            
+        }
+        
+    }
+
+    
+}
+
+
+
+
--- Trainer.cs
+++ Trainer.cs
@@ -2,17 +2,291 @@ namespace mis_221_pa_5_alyork2
 {
     public class Trainer
     {
-        private string ID = "";
-        private string name = "";
-        private string mailingAddress = "";
-        private string emailAddress = "";
+        private int trainerID = 1;
+        private string trainerName = "";
+        private string trainerMailingAddress = "";
+        private string trainerEmailAddress = "";
+        
 
 
-        public Trainer()
+        public Trainer(int ID, string name, string mailingAddress, string emailAddress)
+        {
+            this.trainerID = ID;
+            this.trainerName = name;
+            this.trainerMailingAddress = mailingAddress;
+            this.trainerEmailAddress = emailAddress;
+            TrainerInfo();
+        }
+
+        public Trainer(string name)
+        {
+            this.trainerID = trainerID++;
+            this.trainerName = name;
+            Console.WriteLine("What is the mailing address for this trainer?");
+            this.trainerMailingAddress = Console.ReadLine();
+            Console.WriteLine("What is the email address for this trainer?");
+            this.trainerEmailAddress = Console.ReadLine();
+        }
+
+        public int GetID()
+        {
+            return trainerID;
+        }
+
+        public void SetID(int ID)
+        {
+            this.trainerID = ID;
+        }
+        
+        public string GetName()
+        {
+            return trainerName;
+        }
+
+        public void SetName(string name)
+        {
+            this.trainerName = name;
+        }
+
+        public string GetMailingAddress()
+        {
+            return trainerMailingAddress;
+        }
+
+        public void SetMailingAddress(string mailingAddress)
+        {
+            this.trainerMailingAddress = mailingAddress;
+        }
+
+        public string GetEmailAddress()
+        {
+            return trainerEmailAddress;
+        }
+
+        public void SetEmailAddress(string emailAddress)
+        {
+            this.trainerEmailAddress = emailAddress;
+        }
+
+         public void EditTrainer(string ID, string name, string mailingAddress, string emailAddress)
+        {
+            bool foundTrainer = false;
+            string input;
+            string fileName;
+            string fileText;
+            string[] sections;
+            
 
 
 
 
+
+            Console.WriteLine("What part of the trainer information that you would like to edit?");
+            input = Console.ReadLine();
+
+            Console.WriteLine("What is the file that you would like to edit the trainer in? ");
+            fileName = Console.ReadLine();
+
+            StreamReader fileIn = new StreamReader(fileName);
+            
+            while((fileText = fileIn.ReadLine())!= null)
+            {
+                sections = fileText.Split(' ');
+                if(input == "name")
+                {
+                    if (sections[0] == name)
+                    {
+                        foundTrainer = true;
+                        sections[0] = "";
+                        Console.WriteLine("What is the new name? ");
+                        name = Console.ReadLine();
+                        sections[0] = name;
+                        fileIn.Close();
+                    }
+                    else 
+                    {
+                        foundTrainer = false;
+                    }
+                }
+
+                else if (input == "ID")
+                {
+                    if (sections[1] == ID)
+                    {
+                        foundTrainer = true;
+                        sections[1] = "";
+                        Console.WriteLine("What is the new ID? ");
+                        ID = Console.ReadLine();
+                        sections[1] = ID;
+                        fileIn.Close();
+
+                    }
+                    else 
+                    {
+                        foundTrainer = false;
+                    }
+                }
+                else if(input == "mailing address")
+                {
+                    if(sections[2] == mailingAddress)
+                    {
+                        foundTrainer = true;
+                        sections[2] = "";
+                        Console.WriteLine("What is the new mailing address? ");
+                        mailingAddress = Console.ReadLine();
+                        sections[2] = mailingAddress;
+                        fileIn.Close();
+                    }
+                    else
+                    {
+                        foundTrainer = false;
+                    }
+                }
+            
+                else if(input == "email address")
+                {
+                    if(sections[3] == emailAddress)
+                    {
+                        foundTrainer = true;
+                        sections[3] = "";
+                        Console.WriteLine("What is the new email address? ");
+                        emailAddress = Console.ReadLine();
+                        sections[3] = emailAddress;
+                        fileIn.Close();
+                    }
+                    else
+                    {
+                        foundTrainer = false;
+                    }
+                }
+                else
+                {
+                    foundTrainer = false;
+                    fileIn.Close();
+                }
+
+                if(foundTrainer == true)
+                {
+                    Console.WriteLine("The trainer information has been edited");
+                }
+                else
+                {
+                Console.WriteLine("The trainer was not found");
+                }
+            }
+        }
+        
+        public void DeleteTrainer(string ID, string name, string mailingAddress, string emailAddress)
+        {
+            bool foundTrainer = false;
+            string input;
+            string fileName;
+            string fileText;
+            string[] sections;
+            
+
+
+
+
+
+            Console.WriteLine("What part of the trainer information that you would like to delete?");
+            input = Console.ReadLine();
+
+            Console.WriteLine("What is the file that you would like to edit the trainer in? ");
+            fileName = Console.ReadLine();
+
+            StreamReader fileIn = new StreamReader(fileName);
+            
+            while((fileText = fileIn.ReadLine())!= null)
+            {
+                sections = fileText.Split(' ');
+                if(input == "name")
+                {
+                    if (sections[0] == name)
+                    {
+                        foundTrainer = true;
+                        sections[0] = "";
+                        fileIn.Close();
+                    }
+                    else 
+                    {
+                        foundTrainer = false;
+                    }
+                }
+
+                else if (input == "ID")
+                {
+                    if (sections[1] == ID)
+                    {
+                        foundTrainer = true;
+                        sections[1] = "";
+                        fileIn.Close();
+
+                    }
+                    else 
+                    {
+                        foundTrainer = false;
+                    }
+                }
+                else if(input == "mailing address")
+                {
+                    if(sections[2] == mailingAddress)
+                    {
+                        foundTrainer = true;
+                        sections[2] = "";
+                        fileIn.Close();
+                    }
+                    else
+                    {
+                        foundTrainer = false;
+                    }
+                }
+            
+                else if(input == "email address")
+                {
+                    if(sections[3] == emailAddress)
+                    {
+                        foundTrainer = true;
+                        sections[3] = "";
+                        fileIn.Close();
+                    }
+                    else
+                    {
+                        foundTrainer = false;
+                    }
+                }
+                else
+                {
+                    foundTrainer = false;
+                    fileIn.Close();
+                }
+
+                if(foundTrainer == true)
+                {
+                    Console.WriteLine("The trainer information has been deleted");
+                }
+                else
+                {
+                Console.WriteLine("The trainer was not found");
+                }
+            }
+        }
+        
+
+
+
+        public void TrainerInfo()
+        {
+            string fileName;
+
+            Console.WriteLine("What is the file name that you would like the trainer information in? ");
+            fileName = Console.ReadLine();
+
+            StreamWriter file = new StreamWriter(fileName);
+            file.WriteLine(trainerName + " " + trainerID + " " + trainerMailingAddress + " " + trainerEmailAddress); 
+
+            file.Close();
+        }
 
 
 
