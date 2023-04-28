11f33f7ed1bd312e2f71bf1cf949fc334f88242c
Aiden York <alyork@crimson.ua.edu>
2023-04-27 21:48:41 -0500

    First commit

---
+++ Trainer.cs
@@ -0,0 +1,28 @@
+namespace mis_221_pa_5_alyork2
+{
+    public class Trainer
+    {
+        private string ID = "";
+        private string name = "";
+        private string mailingAddress = "";
+        private string emailAddress = "";
+
+
+        public Trainer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
